eb5767328746683b034a766390811024e34cd54a
Kalyan Vedala <svedala23@gmail.com>
2023-08-09 21:58:59 -0700

    Add components for CreateInstance and InstanceDetails

---
+++ src/components/CreateInstance.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function CreateInstance() {
+  return (
+    <>
+      <h1>Create Instance</h1>
+    </>
+  );
+}
+
+export default CreateInstance;
--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import { useState } from 'react';
 import ListInstances from './ListInstances';
+import InstanceDetails from './InstanceDetails';
+import CreateInstance from './CreateInstance';
 
 function Dashboard() {
+  const [componentToShow, setComponentToShow] = useState("LIST_INSTANCES");
 
   return (
     <div className="dashboard">
       <h1>CloudMessage Dashboard</h1>
-      <ListInstances />
+      {componentToShow === "LIST_INSTANCES" && <ListInstances />}
+      {componentToShow === "INSTANCE_DETAILS" && <InstanceDetails />}
+      {componentToShow === "CREATE_INSTANCE" && <CreateInstance />}
     </div>
   );
 }
