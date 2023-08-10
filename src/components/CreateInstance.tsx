7a8643d42fcc351f5ace896ac01cfcc546ce31a0
Kalyan Vedala <svedala23@gmail.com>
2023-08-10 05:46:57 -0700

    Add CreateInstance form with placeholder for handleSubmit

--- src/components/CreateInstance.tsx
+++ src/components/CreateInstance.tsx
@@ -1,10 +1,28 @@
 import React from 'react';
 
 function CreateInstance({ onClickListInstances}: any) {
+
+  function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+    console.log("form submitted");
+  }
+
   return (
     <>
       <h1>Create Instance</h1>
-      <button onClick={onClickListInstances}>List Instances</button>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <p>Instance Name</p>
+          <input name="name" />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+      <button
+        style={{marginTop: "100px"}}
+        onClick={onClickListInstances}
+      >
+        List Instances
+      </button>
     </>
   );
 }
