b0dce7b26335b89dc0ed3be785405454f6baf721
Kalyan Vedala <svedala23@gmail.com>
2023-08-10 06:02:01 -0700

    CreateInstance save state - wip

--- src/components/CreateInstance.tsx
+++ src/components/CreateInstance.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
+import { useState } from 'react';
 
 function CreateInstance({ onClickListInstances}: any) {
+  const [instName, setInstName] = useState("");
 
-  function handleSubmit(e: React.FormEvent) {
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
-    console.log("form submitted");
+    console.log(e.currentTarget.name.valueOf)
+    setInstName(e.currentTarget.name.valueOf)
   }
 
   return (
     <>
       <h1>Create Instance</h1>
+
       <form onSubmit={handleSubmit}>
         <label>
           <p>Instance Name</p>
@@ -17,12 +21,14 @@ function CreateInstance({ onClickListInstances}: any) {
         </label>
         <button type="submit">Submit</button>
       </form>
+
       <button
         style={{marginTop: "100px"}}
         onClick={onClickListInstances}
       >
         List Instances
       </button>
+
     </>
   );
 }
