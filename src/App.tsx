0a972ed571a888fb804e504bf6b536c6a245aa2e
Kalyan Vedala <svedala23@gmail.com>
2023-08-11 06:02:10 -0700

    Put entire dashboard behind authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,33 @@
 import React from 'react';
+import { useAuth0 } from "@auth0/auth0-react";
 import './App.css';
 import Dashboard from './components/Dashboard';
+import LoginButton from './components/login';
+import LogoutButton from './components/logout';
+import Profile from './components/profile';
 
 function App() {
+  const { isAuthenticated, isLoading } = useAuth0();
+
+  if (isLoading) {
+    return <div>Loading ...</div>;
+  }
+
   return (
     <div className="App">
-      <Dashboard/>
+      { !isAuthenticated
+        ? <LoginButton />
+        : <>
+            <LogoutButton />
+            <hr />
+            <h3>Profile Information</h3>
+            <Profile></Profile>
+            <hr />
+            <Dashboard/>
+          </>
+      }
     </div>
-  );
+  )
 }
 
 export default App;
