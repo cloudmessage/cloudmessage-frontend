08ca224dc7cbdc2c87d8c17cd4e173a741f719ce
Kalyan Vedala <svedala23@gmail.com>
2023-08-26 09:40:19 -0700

    Use environment variables

--- src/index.tsx
+++ src/index.tsx
@@ -3,17 +3,22 @@ import ReactDOM from 'react-dom/client';
 import { Auth0Provider } from '@auth0/auth0-react';
 import App from './App';
 
+const AUTH0_DOMAIN = process.env.REACT_APP_AUTH0_DOMAIN || '';
+const AUTH0_CLIENT_ID = process.env.REACT_APP_AUTH0_CLIENT_ID || '';
+const AUTH0_REDIRECT_URI = process.env.REACT_APP_AUTH0_REDIRECT_URI || '';
+const AUTH0_AUDIENCE = process.env.REACT_APP_AUTH0_AUDIENCE || '';
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
     <Auth0Provider
-      domain="cloudmessage.us.auth0.com"
-      clientId="irjJRLxV407Z5xCE35ZxmjaUAshzxKi0"
+      domain={AUTH0_DOMAIN}
+      clientId={AUTH0_CLIENT_ID}
       authorizationParams={{
-        redirect_uri: 'http://localhost:3006',
-        audience: "https://cloudmessage.com"
+        redirect_uri: AUTH0_REDIRECT_URI,
+        audience: AUTH0_AUDIENCE
       }}
     >
       <App />
