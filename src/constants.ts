3f2dbc710144d56bbdc84ade5f5ea0b9a362af9f
Kalyan Vedala <svedala23@gmail.com>
2023-08-27 06:08:25 -0700

    Create separate constants file

---
+++ src/constants.ts
@@ -0,0 +1,5 @@
+export const AUTH0_DOMAIN = process.env.REACT_APP_AUTH0_DOMAIN || '';
+export const AUTH0_CLIENT_ID = process.env.REACT_APP_AUTH0_CLIENT_ID || '';
+export const AUTH0_REDIRECT_URI = process.env.REACT_APP_AUTH0_REDIRECT_URI || '';
+export const AUTH0_AUDIENCE = process.env.REACT_APP_AUTH0_AUDIENCE || '';
+export const CLOUDMESSAGE_API_URL = process.env.REACT_APP_CLOUDMESSAGE_API_URL || '';
