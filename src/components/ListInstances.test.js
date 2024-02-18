fff5543b3335fb70946f79f351302a4682ddd519
Kalyan Vedala <svedala23@gmail.com>
2024-02-18 10:09:38 -0800

    Basic passing test, but data not rendered yet

--- src/components/ListInstances.test.js
+++ src/components/ListInstances.test.js
@@ -2,10 +2,30 @@ import { render, screen } from '@testing-library/react';
 import ListInstances from './ListInstances';
 import '@testing-library/jest-dom/extend-expect';
 
-test('renders the ListInstances component', () => {
-  render(
-      <ListInstances />
-  );
+jest.mock('axios', () => {
+  return {
+    get: () => {
+      return Promise.resolve({
+        data: [
+          {
+            id: 10,
+            name: "item10"
+          },
+          {
+            id: 20,
+            name: "item20"
+          }
+        ]
+      });
+    }
+  }
+});
 
-  expect(screen.getByRole("heading")).toHaveTextContent("Instances");
+test('renders the ListInstances component', async () => {
+
+  await render(<ListInstances />)
+
+  expect(await screen.findByRole("heading")).toHaveTextContent("Instances");
+  // expect(await screen.findAllByRole("list")).toHaveLength(1);
+
 });
