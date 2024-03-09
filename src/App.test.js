9dc4891f9db0f8e66f85d496beb239fda273eeac
Kalyan Vedala <svedala23@gmail.com>
2024-03-09 05:57:13 -0800

    Update App unit test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,88 @@
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import App from './App';
 
-test.skip('renders the main page', () => {
+jest.mock("./components/login", () => {
+  return {
+    __esModule: true,
+    default: function () {
+      return <h1>LoginButton Mock</h1>
+    }
+  };
+});
+
+jest.mock("./components/profile", () => {
+  return {
+    __esModule: true,
+    default: function () {
+      return <h1>Profile Mock</h1>
+    }
+  };
+});
+
+jest.mock("./components/CreateInstance", () => {
+  return {
+    __esModule: true,
+    default: function () {
+      return <h1>CreateInstance Mock</h1>
+    }
+  };
+});
+
+jest.mock("./components/Dashboard", () => {
+  return {
+    __esModule: true,
+    default: function () {
+      return <h1>Dashboard Mock</h1>
+    }
+  };
+});
+
+jest.mock("./components/InstanceDetails", () => {
+  return {
+    __esModule: true,
+    default: function () {
+      return <h1>InstanceDetails Mock</h1>
+    }
+  };
+});
+
+jest.mock("./components/PageNotFound", () => {
+  return {
+    __esModule: true,
+    default: function () {
+      return <h1>PageNotFound Mock</h1>
+    }
+  };
+});
+
+jest.mock("./components/logout", () => {
+  return {
+    __esModule: true,
+    default: function () {
+      return <h1>LogoutButton Mock</h1>
+    }
+  };
+});
+
+jest.mock("@auth0/auth0-react", () => {
+  return {
+    __esModule: true,
+    useAuth0: function() {
+      return {
+        isAuthenticated: false,
+        isLoading: false
+      }
+    }
+  };
+});
+
+test('renders the main page', async () => {
   render(<App />);
 
-  expect(screen.getAllByRole("button", {hidden: true})).toHaveTextContent(/CloudMessage Dashboard/);
+  const headings = await screen.findAllByRole("heading");
+
+  expect(headings).toHaveLength(2);
+  expect(headings[0]).toHaveTextContent("LoginButton Mock");
+  expect(headings[1]).toHaveTextContent("Dashboard Mock");
 });
